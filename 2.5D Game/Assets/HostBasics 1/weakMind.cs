29f929adfcf155e11375de006d44b24239fb2634
Sam <crozsam2003@gmail.com>
2025-11-11 13:09:45 +0000

    added weakmind script and photon physics

---
+++ 2.5D Game/Assets/HostBasics 1/weakMind.cs
@@ -0,0 +1,18 @@
+using Fusion;
+using UnityEngine;
+
+public class weakMind : MonoBehaviour
+{
+    public NetworkPrefabRef creatureType;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
