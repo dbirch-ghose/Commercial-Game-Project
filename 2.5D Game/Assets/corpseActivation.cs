3e304499ac40249cd251b5c00a34c35ebb4093aa
Sam <crozsam2003@gmail.com>
2025-12-02 15:23:43 +0000

    switched to swing animation over lerp

--- 2.5D Game/Assets/corpseActivation.cs
+++ 2.5D Game/Assets/corpseActivation.cs
@@ -5,12 +5,10 @@ public class corpseActivation : NetworkBehaviour
 {
     public NetworkObject Hanger;
     public bool activate = false;
+    public Animator Swinger;
 
     public Transform cubeStart;
     public Transform cubeEnd;
-    private float framesElapsed = 0;
-    private bool start = false;
-    public float totalFrames = 60;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,18 +20,7 @@ public class corpseActivation : NetworkBehaviour
     {
         if (activate == true)
         {
-            if (!start)
-            {
-                framesElapsed = 0;
-                start = true;
-            }
-            float ratio = framesElapsed / totalFrames;
-            if (ratio > 1)
-            {
-                ratio = 1;
-            }
-            Hanger.transform.position=Vector3.Lerp(cubeStart.position, cubeEnd.position, ratio);
-            framesElapsed += 1;
+            Swinger.SetBool("activated", true);
         }
     }
 
