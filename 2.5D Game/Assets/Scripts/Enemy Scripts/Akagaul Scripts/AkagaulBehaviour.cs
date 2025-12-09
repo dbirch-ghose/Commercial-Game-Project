bf9cf83da5ae1b6be6108f0e524754f1c558f248
don <donny77776@gmail.com>
2025-12-09 17:28:04 +0000

    added another horse variant

--- 2.5D Game/Assets/Scripts/Enemy Scripts/Akagaul Scripts/AkagaulBehaviour.cs
+++ 2.5D Game/Assets/Scripts/Enemy Scripts/Akagaul Scripts/AkagaulBehaviour.cs
@@ -32,6 +32,7 @@ public class AkagaulBehaviour : NetworkBehaviour
 
     //-------------------HORSE----------------------
     public NetworkObject Horse;
+    public NetworkObject Horse2;
     //public float spawnZ = 2f;       // where along Y to spawn (center)
     public float targetPos;
     // where to despawn, THESE BOUNDS MUST BE CHANGED TO FIT THE SIZE OF THE ROOM
@@ -299,9 +300,18 @@ public class AkagaulBehaviour : NetworkBehaviour
             //Vector3 spawnPos = new Vector3(rightSpawnX, 1.5f, targetPos); //spawn horse on the right
 
             NetworkObject horse = Runner.Spawn(Horse, spawnPos, Quaternion.identity);
+            NetworkObject horse2 = Runner.Spawn(Horse2, spawnPos, Quaternion.identity);
 
-            horse.GetComponent<HorseBehaviour>().SetDirection(spawnLeft ? 1 : -1); //chooses movement direction based of the random spawn location
+            int randHorse = UnityEngine.Random.Range(0, 2);  
+            if (randHorse == 0)
+            {
+                horse.GetComponent<HorseBehaviour>().SetDirection(spawnLeft ? 1 : -1); //chooses movement direction based of the random spawn location
+            }
+            else
+            {
+                horse2.GetComponent<HorseBehaviour>().SetDirection(spawnLeft ? 1 : -1); //chooses movement direction based of the random spawn location
 
+            }
             yield return new WaitForSeconds(2f); // duration of attack
             horseCount ++;
         }
