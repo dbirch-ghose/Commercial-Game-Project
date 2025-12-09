0e336f722e37159d09119f5d8f4c0243b8b6ff90
don <donny77776@gmail.com>
2025-12-09 18:44:59 +0000

    made acid vial script networked

--- 2.5D Game/Assets/Scripts/Enemy Scripts/Akagaul Scripts/AcidProjectileBehaviour.cs
+++ 2.5D Game/Assets/Scripts/Enemy Scripts/Akagaul Scripts/AcidProjectileBehaviour.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using UnityEngine;
-
-public class AcidProjectileBehaviour : MonoBehaviour
+using Fusion;
+public class AcidProjectileBehaviour : NetworkBehaviour
 {
     public GameObject particlePrefab;
     private GameObject particles;
