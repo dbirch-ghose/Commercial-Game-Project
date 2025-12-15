eb2ce0fd7d982323d9a3755003e3cdb1259d6f1c
Sam <crozsam2003@gmail.com>
2025-12-15 16:52:50 +0000

    added vignette changes

--- 2.5D Game/Assets/Scripts/Player Scripts/Player Health.cs
+++ 2.5D Game/Assets/Scripts/Player Scripts/Player Health.cs
@@ -1,21 +1,50 @@
 using Fusion;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class PlayerHealth : NetworkBehaviour
 {
     [Networked] public int health { get; set; }
     public int maxHealth = 3;
+    public Volume pp;
+    private Vignette vignette;
+    private float vignetteProgress;
+    private bool vignetteActivate = false;
+    private bool firstFrame = false;
+    private float mathness;
 
     public override void Spawned()
     {
         if (Object.HasStateAuthority)
             health = maxHealth;
+        pp = GameObject.FindWithTag("pp").GetComponent<Volume>();
+        pp.profile = Instantiate(pp.profile);
+
+        if (pp.profile.TryGet(out vignette))
+        {
+            vignette.intensity.overrideState = true;
+        }
     }
 
     public override void FixedUpdateNetwork()
     {
         if (!Object.HasStateAuthority)
             return;
+        if (firstFrame)
+        {
+            Debug.Log("First frame is true");
+            vignetteProgress = 0;
+            firstFrame = false;
+        }
+        if (vignetteActivate && vignetteProgress < 1)
+        {
+            Debug.Log("running mathness");
+            mathness = 0.35f * (-(2f * vignetteProgress - 1f) * (2f * vignetteProgress - 1f) + 1f);
+            vignette.intensity.value = mathness;
+            vignetteProgress += 0.02f;
+        }
 
         if (health < 0)
             health = 0;
@@ -27,8 +56,11 @@ public class PlayerHealth : NetworkBehaviour
     {
         if (health <= 0)
             return;
-
+        Debug.Log("take damage");
+        vignetteActivate = true;
+        firstFrame = true;
         health -= damage;
         Debug.Log($"Player took {damage} damage. Health now {health}");
     }
+
 }
