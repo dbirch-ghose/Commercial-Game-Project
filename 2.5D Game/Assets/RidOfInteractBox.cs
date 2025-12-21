cc7bceb281093379d3ac72e93cbf4d039941a89e
Sam <crozsam2003@gmail.com>
2025-12-21 00:51:34 +0000

    Interact button finally works as fully intended
    needs beautifying and is currently being hidden behind the dialogue box although that's kinda fine

--- 2.5D Game/Assets/RidOfInteractBox.cs
+++ 2.5D Game/Assets/RidOfInteractBox.cs
@@ -1,16 +1,36 @@
+using PixelCrushers.DialogueSystem;
 using UnityEngine;
+
 
 public class RidOfInteractBox : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public LiterallyJustHoldsAReferenceToThisText interactPrompt;
+    private Selector selector;
+    private GameObject prompt;
     void Start()
     {
-        
+        interactPrompt = FindFirstObjectByType<LiterallyJustHoldsAReferenceToThisText>(FindObjectsInactive.Include);
+        prompt = interactPrompt.text;
+        selector = GetComponent<Selector>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var usable = selector.CurrentUsable;
+
+        if (usable == null)
+        {
+            prompt.SetActive(false);
+            return;
+        }
+
+        float distance = Vector3.Distance(
+            selector.transform.position,
+            usable.transform.position
+        );
+
+        prompt.SetActive(distance <= usable.maxUseDistance);
     }
 }
