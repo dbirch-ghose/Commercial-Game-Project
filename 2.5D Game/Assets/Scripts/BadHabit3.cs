8f1e39695fc985ac9e6b3a02fb95f94701852743
Sam <crozsam2003@gmail.com>
2026-01-17 15:30:59 +0000

    library puzzle

---
+++ 2.5D Game/Assets/Scripts/BadHabit3.cs
@@ -0,0 +1,18 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class BadHabit3 : MonoBehaviour
+{
+    public bool down;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        down = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
