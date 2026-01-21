510e7ad1af885ab1932d13c6fc358f62dc045d00
Sam <crozsam2003@gmail.com>
2026-01-21 17:46:15 +0000

    awdh

--- 2.5D Game/Assets/Scripts/Camera/SwitchCameraPositions.cs
+++ 2.5D Game/Assets/Scripts/Camera/SwitchCameraPositions.cs
@@ -1,7 +1,6 @@
-using NUnit.Framework;
+
 using UnityEngine;
-using UnityEngine.UI;
-using static UnityEditor.Experimental.GraphView.GraphView;
+
 
 
 public class SwitchCameraPosition : MonoBehaviour
