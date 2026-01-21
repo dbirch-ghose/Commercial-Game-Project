c73701c1ec6d08fde92bb6d75f8898d86e14d3bb
Sam <crozsam2003@gmail.com>
2026-01-21 19:31:05 +0000

    couple changes

--- 2.5D Game/Assets/Scripts/Player Scripts/Boulder Controller.cs
+++ 2.5D Game/Assets/Scripts/Player Scripts/Boulder Controller.cs
@@ -3,32 +3,32 @@ using Fusion;
 
 public class BoulderController : NetworkBehaviour
 {
-    private bool picked = false;
-    public Animator animator;
-    public override void Spawned()
-    {
-        animator = GetComponent<Animator>();
-    }
+    //private bool picked = false;
+    //public Animator animator;
+    //public override void Spawned()
+    //{
+    //    animator = GetComponent<Animator>();
+    //}
 
-    [Networked] public bool Open { get; set; }
-    public override void Render()
-    {
-        if (picked)
-            return;
-        animator.SetBool("isPickingUp", Open);
-        picked = Open;
+    //[Networked] public bool Open { get; set; }
+    //public override void Render()
+    //{
+    //    if (picked)
+    //        return;
+    //    //animator.SetBool("isPickingUp", Open);
+    //    picked = Open;
 
-        if (Open) GetComponent<Collider>().enabled = false;
+    //    if (Open) GetComponent<Collider>().enabled = false;
 
-    }
+    //}
 
-    [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
-    public void RPC_BoulderAnim()
-    {
-        if (Open)
-            return;
-        Open = true;
-    }
+    //[Rpc(RpcSources.All, RpcTargets.StateAuthority)]
+    //public void RPC_BoulderAnim()
+    //{
+    //    if (Open)
+    //        return;
+    //    Open = true;
+    //}
 
 
 }
