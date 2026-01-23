8916e09b5fa57224ee669f640f76aab3d23cf0de
Sam <crozsam2003@gmail.com>
2026-01-23 00:05:11 +0000

    disable selectors before ackagul to stop a bug

---
+++ 2.5D Game/Assets/selectorDisabler.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using Fusion;
+using PixelCrushers.DialogueSystem;
+
+public class selectorDisabler : NetworkBehaviour
+{
+    Selector[] selectors;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public override void Spawned()
+    {
+        
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        RPC_disableSelector();
+    }
+
+    [Rpc(RpcSources.All, RpcTargets.All)]
+    public void RPC_disableSelector()
+    {
+        selectors = FindObjectsByType<Selector>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+        foreach (Selector selector in selectors)
+        {
+            selector.enabled = false;
+        }
+    }
+}
