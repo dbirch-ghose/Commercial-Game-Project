8eeb6342e2db68171eb638af13e5fd3d7ca06250
don <donny77776@gmail.com>
2026-05-11 15:00:52 +0100

    tweaked OT door cam trigger

--- 2.5D Game/Assets/Scripts/Room Switching/OT2Trigger.cs
+++ 2.5D Game/Assets/Scripts/Room Switching/OT2Trigger.cs
@@ -1,22 +1,46 @@
-
-
-    using Fusion;
 using UnityEngine;
+using Fusion;
+using System.Collections;
 
 public class OT2Trigger : NetworkBehaviour
 {
+    public OTTrigger oTTrigger;
     public SwitchCameraPosition switchCameraPosition;
-    public Transform CamPos;
+    public Transform CamPos1;
+    public Transform CamPos2;
+    public bool inOT2 = false;
+
+    public float cooldown = 0.5f;
+    private bool onCooldown = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!Object.HasStateAuthority) return;
-        if (!other.CompareTag("Player")) return;
+        var playerNO = other.GetComponentInParent<NetworkObject>();
+        if (playerNO == null)
+            return;
 
+        if (!playerNO.HasInputAuthority)
+            return;
 
-        switchCameraPosition.MoveCamera(CamPos);
+        if (onCooldown)
+            return;
 
+        StartCoroutine(Cooldown());
 
+        if (oTTrigger.inOT == true)
+        {
+            switchCameraPosition.MoveCamera(CamPos1);
+            inOT2 = true;
+            string[] rooms = { "Operating Theatre", "Enemy" };
+            switchCameraPosition.ShowRoom(rooms);
+        }
+       
+    }
+    private IEnumerator Cooldown()
+    {
+        onCooldown = true;
+        yield return new WaitForSeconds(cooldown);
+        onCooldown = false;
     }
 }
 
